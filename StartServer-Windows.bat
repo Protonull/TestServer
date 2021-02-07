a68f7017956d229796814c5fa1749ebbca5da94e
Alexander <protonull@protonmail.com>
2021-02-07 18:04:16 +0000

    Lotta changes
    • Switched from a proprietary jar to bash+wget
    
    • Now uses Aikar's flags by default
    
    • Removed extraneous level data
    
    • Disabled day/night cycle by default
    
    • Disabled weather cycle by default

--- StartServer-Windows.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Minecraft Server
-cd %~dp0
-:restart
-cls
-java -jar setup.jar
-java -Xmx2000M -Xms2000M -jar paper.jar nogui
-pause
-goto restart
---
+++ StartServer.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit
+start_server() {
+    clear
+    wget -nc -O "paper.jar" "https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/465/downloads/paper-1.16.5-465.jar"
+    wget -nc -O "plugins/PlugMan-2.1.7.jar" "https://media.forgecdn.net/files/2861/749/PlugMan.jar"
+    wget -nc -O "plugins/ViaVersion-3.2.1.jar" "https://github.com/ViaVersion/ViaVersion/releases/download/3.2.1/ViaVersion-3.2.1.jar"
+    wget -nc -O "plugins/ViaBackwards-3.2.0.jar" "https://github.com/ViaVersion/ViaBackwards/releases/download/3.2.0/ViaBackwards-3.2.0.jar"
+    wget -nc -O "plugins/WorldEdit-7.2.2.jar" "https://dev.bukkit.org/projects/worldedit/files/3172946"
+    java -Xmx2000M -Xms2000M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paperclip.jar nogui
+    read -p "Press any key to continue..."
+    start_server
+}
+start_server
