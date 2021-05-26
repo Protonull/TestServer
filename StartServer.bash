81837a4f801a95fa68a9244cb5cac3e03badb4d0
Alexander <protonull@protonmail.com>
2021-05-26 08:01:28 +0100

    Use better "Press any key to continue…"

--- StartServer.bash
+++ StartServer.bash
@@ -21,7 +21,7 @@ start_server() {
     download "ViaBackwards" "plugins/ViaBackwards-3.2.0.jar" "https://github.com/ViaVersion/ViaBackwards/releases/download/3.2.0/ViaBackwards-3.2.0.jar"
     download "WorldEdit" "plugins/WorldEdit-7.2.5.jar" "https://media.forgecdn.net/files/3283/695/worldedit-bukkit-7.2.5-dist.jar"
     java -Xmx2000M -Xms2000M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar nogui
-    read -p "Press any key to continue..."
+    read -s -n 1 -p "Press any key to continue..."
     start_server
 }
 start_server
