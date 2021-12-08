4884e27fc07df29ab4fcfb4903c38ce6b500c9b1
Alexander <protonull@protonmail.com>
2021-12-08 12:43:41 +0000

    Update ViaVersion, ViaBackwards, and WorldEdit versions

--- StartServer.bash
+++ StartServer.bash
@@ -16,9 +16,9 @@ start_server() {
     clear
     download "Tuinity" "tuinity.jar" "https://ci.codemc.io/job/Spottedleaf/job/Tuinity/242/artifact/tuinity-paperclip.jar"
     download "ProtocolLib" "plugins/ProtocolLib-4.7.0.jar" "https://github.com/dmulloy2/ProtocolLib/releases/download/4.7.0/ProtocolLib.jar"
-    download "ViaVersion" "plugins/ViaVersion-4.0.1.jar" "https://github.com/ViaVersion/ViaVersion/releases/download/4.0.1/ViaVersion-4.0.1.jar"
-    download "ViaBackwards" "plugins/ViaBackwards-4.0.1.jar" "https://github.com/ViaVersion/ViaBackwards/releases/download/4.0.1/ViaBackwards-4.0.1.jar"
-    download "WorldEdit" "plugins/WorldEdit-7.2.6.jar" "https://media.forgecdn.net/files/3433/988/worldedit-bukkit-7.2.6.jar"
+    download "ViaVersion" "plugins/ViaVersion-4.1.1.jar" "https://github.com/ViaVersion/ViaVersion/releases/download/4.1.1/ViaVersion-4.1.1.jar"
+    download "ViaBackwards" "plugins/ViaBackwards-4.1.1.jar" "https://github.com/ViaVersion/ViaBackwards/releases/download/4.1.1/ViaBackwards-4.1.1.jar"
+    download "WorldEdit" "plugins/WorldEdit-7.2.7.jar" "https://media.forgecdn.net/files/3502/99/worldedit-bukkit-7.2.7.jar"
     java -Xmx2000M -Xms2000M \
         -XX:+UseG1GC \
         -XX:+ParallelRefProcEnabled \
