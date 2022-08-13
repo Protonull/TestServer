e867ac239e0ce58f1b18807e8c0f89c68c042817
Alexander <protonull@protonmail.com>
2022-08-13 02:00:46 +0100

    Update ViaVersion to 4.4.1

--- StartServer.bash
+++ StartServer.bash
@@ -18,7 +18,7 @@ start_server() {
     # ProtocolLib -> https://github.com/dmulloy2/ProtocolLib/
     download "ProtocolLib" "plugins/ProtocolLib-4.8.0.jar" "https://github.com/dmulloy2/ProtocolLib/releases/download/4.8.0/ProtocolLib.jar"
     # ViaVersion -> https://github.com/ViaVersion/ViaVersion/
-    download "ViaVersion" "plugins/ViaVersion-4.3.1.jar" "https://github.com/ViaVersion/ViaVersion/releases/download/4.3.1/ViaVersion-4.3.1.jar"
+    download "ViaVersion" "plugins/ViaVersion-4.4.1.jar" "https://github.com/ViaVersion/ViaVersion/releases/download/4.4.1/ViaVersion-4.4.1.jar"
     # ViaBackwards -> https://github.com/ViaVersion/ViaBackwards/
     download "ViaBackwards" "plugins/ViaBackwards-4.3.0.jar" "https://github.com/ViaVersion/ViaBackwards/releases/download/4.3.0/ViaBackwards-4.3.0.jar"
     # WorldEdit for Bukkit -> https://dev.bukkit.org/projects/worldedit/files/
