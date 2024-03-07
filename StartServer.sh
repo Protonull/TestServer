572349c6a8de3ac9d6da9c11a91df2205d9aa103
Alexander <protonull@protonmail.com>
2024-03-07 03:09:17 +0000

    Update ViaBackwards to 4.9.2

--- StartServer.sh
+++ StartServer.sh
@@ -21,7 +21,7 @@ start_server() {
     # ViaVersion -> https://github.com/ViaVersion/ViaVersion/
     download "ViaVersion" "plugins/ViaVersion-4.9.3.jar" "https://github.com/ViaVersion/ViaVersion/releases/download/4.9.3/ViaVersion-4.9.3.jar"
     # ViaBackwards -> https://github.com/ViaVersion/ViaBackwards/
-    download "ViaBackwards" "plugins/ViaBackwards-4.9.1.jar" "https://github.com/ViaVersion/ViaBackwards/releases/download/4.9.1/ViaBackwards-4.9.1.jar"
+    download "ViaBackwards" "plugins/ViaBackwards-4.9.2.jar" "https://github.com/ViaVersion/ViaBackwards/releases/download/4.9.2/ViaBackwards-4.9.2.jar"
     # WorldEdit for Bukkit -> https://dev.bukkit.org/projects/worldedit/files/
     download "WorldEdit" "plugins/WorldEdit-7.2.19.jar" "https://mediafilez.forgecdn.net/files/5077/477/worldedit-bukkit-7.2.19.jar"
     # PlugManX -> https://github.com/TheBlackEntity/PlugMan/
