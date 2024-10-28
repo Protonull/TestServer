20b2463471e76c9d75cd25044690f986824a1533
Alexander <protonull@protonmail.com>
2024-10-28 09:56:06 +0000

    Update ViaVersion and ViaBackwards to 5.1.0

--- StartServer.sh
+++ StartServer.sh
@@ -26,9 +26,9 @@ start_server() {
     # ProtocolLib -> https://github.com/dmulloy2/ProtocolLib/
     download "ProtocolLib" "plugins/ProtocolLib-5.3.0.jar" "https://github.com/dmulloy2/ProtocolLib/releases/download/5.3.0/ProtocolLib.jar"
     # ViaVersion -> https://github.com/ViaVersion/ViaVersion/
-    download "ViaVersion" "plugins/ViaVersion-5.0.3.jar" "https://github.com/ViaVersion/ViaVersion/releases/download/5.0.3/ViaVersion-5.0.3.jar"
+    download "ViaVersion" "plugins/ViaVersion-5.1.0.jar" "https://github.com/ViaVersion/ViaVersion/releases/download/5.1.0/ViaVersion-5.1.0.jar"
     # ViaBackwards -> https://github.com/ViaVersion/ViaBackwards/
-    download "ViaBackwards" "plugins/ViaBackwards-5.0.3.jar" "https://github.com/ViaVersion/ViaBackwards/releases/download/5.0.3/ViaBackwards-5.0.3.jar"
+    download "ViaBackwards" "plugins/ViaBackwards-5.1.0.jar" "https://github.com/ViaVersion/ViaBackwards/releases/download/5.1.0/ViaBackwards-5.1.0.jar"
     # WorldEdit for Bukkit -> https://dev.bukkit.org/projects/worldedit/files/
     download "WorldEdit" "plugins/WorldEdit-7.3.6.jar" "https://mediafilez.forgecdn.net/files/5613/179/worldedit-bukkit-7.3.6.jar"
     java -Xmx2000M -Xms2000M \
